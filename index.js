dccf07c404a3e4752140d6ab7d26f09074cd92cf
Austin Cripe <aaustinc@live.com>
2023-09-26 12:05:27 -0400

    built out front end

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const pg = require('pg');
-const client = new pg.Client('postgres://localhost/fullstack_template_db');
+const client = new pg.Client('postgres://localhost/vacation_planner_db');
 const express = require('express');
 const app = express();
 const path = require('path');
@@ -16,12 +16,80 @@ app.get('/dist/main.js.map', (req, res)=> res.sendFile(reactSourceMap));
 const styleSheet = path.join(__dirname, 'styles.css');
 app.get('/styles.css', (req, res)=> res.sendFile(styleSheet));
 
+
+app.get('/api/users', async(req,res,next)=>{
+  try {
+    const SQL = `
+    SELECT * FROM users
+    `;
+    const response = await client.query(SQL)
+    res.send(response.rows)
+  } catch (error) {
+    next(error)
+  }
+})
+app.get('/api/places', async(req,res,next)=>{
+  try {
+    const SQL = `
+    SELECT * FROM places
+    `;
+    const response = await client.query(SQL)
+    res.send(response.rows)
+  } catch (error) {
+    next(error)
+  }
+})
+app.get('/api/vacations', async(req,res,next)=>{
+  try {
+    const SQL = `
+    SELECT * FROM vacations
+    `;
+    const response = await client.query(SQL)
+    res.send(response.rows)
+  } catch (error) {
+    next(error)
+  }
+})
+
 const init = async()=> {
   await client.connect();
   console.log('connected to database');
   const SQL = `
-    SQL SETUP AND SEED
+    DROP TABLE IF EXISTS vacations;
+    DROP TABLE IF EXISTS users;
+    DROP TABLE IF EXISTS places;
+    CREATE TABLE users(
+      id SERIAL PRIMARY KEY,
+      name VARCHAR(100) UNIQUE
+    );
+    CREATE TABLE places(
+      id SERIAL PRIMARY KEY,
+      name VARCHAR(100)
+    );
+    CREATE TABLE vacations(
+      id SERIAL PRIMARY KEY,
+      place_id INT REFERENCES places(id) NOT NULL,
+      user_id INT REFERENCES users(id) NOT NULL,
+      created_at TIMESTAMP DEFAULT now()
+    );
+    INSERT INTO users (name) VALUES ('moe');
+    INSERT INTO users (name) VALUES ('larry');
+    INSERT INTO users (name) VALUES ('chuck');
+    INSERT INTO users (name) VALUES ('ethyl');
+    INSERT INTO users (name) VALUES ('lucy');
+
+    INSERT INTO places (name) VALUES ('Iceland');
+    INSERT INTO places (name) VALUES ('Costa Rica');
+    INSERT INTO places (name) VALUES ('Dallas');
+    INSERT INTO places (name) VALUES ('Montana');
+
+    INSERT INTO vacations(user_id, place_id) VALUES (
+      (SELECT id FROM users WHERE name='lucy'),
+      (SELECT id FROM places WHERE name='Iceland')
+      );
+
   `;
+  await client.query(SQL)
   console.log('create your tables and seed data');
 
   const port = process.env.PORT || 3000;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,104 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const Users = ({ users, vacations })=> {
+
+  return (
+    <div>
+      <h2>Users ({ users.length })</h2>
+      <ul>
+        {
+          users.map(user=>{
+            return (
+              <li key={user.id}>
+                { user.name }
+                ({ vacations.filter(vaca=> vaca.user_id === user.id).length })
+              </li>
+            )
+          })
+        }
+      </ul>
+    </div>
+  )
+}
+const Vacations = ({ vacations, places })=> {
+
+  return (
+    <div>
+      <h2>Vacations ({ vacations.length })</h2>
+      <ul>
+        {
+          vacations.map(vaca=>{
+            const place = places.find(place=>place.id === vaca.place_id)
+            return (
+              <li key={vaca.id}>
+                { new Date(vaca.created_at).toLocaleString() }
+                <div>
+                  to: { place ? place.name : null}
+                </div>
+              </li>
+            )
+          })
+        }
+      </ul>
+    </div>
+  )
+}
+const Places = ({ places, vacations })=> {
+
+  return (
+    <div>
+      <h2>Places ({ places.length })</h2>
+      <ul>
+        {
+          places.map(place=>{
+            return (
+              <li key={place.id}>
+                { place.name }
+                ({ vacations.filter(vaca=> vaca.place_id === place.id).length })
+                </li>
+            )
+          })
+        }
+      </ul>
+    </div>
+  )
+}
 
 const App = ()=> {
+  const [users, setUsers]= useState([])
+  const [vacations, setVacations]= useState([])
+  const [places, setPlaces]= useState([])
+
+  useEffect(()=>{
+    const fetchUsers = async()=>{
+      const response = await axios.get('/api/users')
+      setUsers(response.data)
+    }
+    const fetchVacations = async()=>{
+      const response = await axios.get('/api/vacations')
+      setVacations(response.data)
+    }
+    const fetchPlaces = async()=>{
+      const response = await axios.get('/api/places')
+      setPlaces(response.data)
+    }
+    fetchPlaces()
+    fetchUsers()
+    fetchVacations()
+  }, [])
+
   return (
-    <h1>FullStack Template</h1>
+    <div>
+      <h1>FullStack Template</h1>
+      <main>
+      <Vacations vacations={vacations} places={places}/>
+      <Users users={users} vacations={vacations}/>
+      <Places places={places} vacations={vacations}/>
+      </main>
+    </div>
   );
 };
 
